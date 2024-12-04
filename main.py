20571ba35babdd6a0fd255ff5806858f6ef306cf
shkim <shkim@broadcns.com>
2024-12-04 16:17:48 +0900

    main파일 생성

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    print('main Func Process')
+    f = open("절대경로를 어떻게 설정하면되지", 'r')
+    data = f.read()
+    print(data)
+    f.close()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
