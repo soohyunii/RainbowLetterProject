93c27068b8b3582f9c2704d900ca980a736e2593
shkim <shkim@broadcns.com>
2024-12-23 16:37:21 +0900

    pilow issue

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RainbowLetterProject
 무지개글자 토이 프로젝트
+
+
+참고자료 : https://blog.naver.com/monkey5255/221594654820
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-def main():
-    print('main Func Process')
-    f = open("절대경로를 어떻게 설정하면되지", 'r')
-    data = f.read()
-    print(data)
-    f.close()
+from pathlib import Path
+from PIL import Image, ImageDraw, ImageFont
 
+def main():
+    # 사용자가 설정한 경로로 찾아가서 파일 찾음
+    user_path = "C:/Users/김수현/Desktop/rainbow.txt"
+    f = open(user_path, encoding='UTF-8')
+    data = f.read()
+    
+    #메모장(txt)글씨색을 변경하는 기능은 없음...png저장으로 대체
+    # with open(user_path, 'w', encoding='UTF-8') as file:
+    #     file.truncate(0)
+    #     file.write(data_rainbow_png)
+    # f.close()
+    
+    
 
 
 
