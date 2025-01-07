bcf6c3769f5b951df5f2c60c95f1a058add4f984
shkim <shkim@broadcns.com>
2025-01-07 13:22:34 +0900

    setting canvas

--- main.py
+++ main.py
@@ -16,7 +16,26 @@ def main():
     # f.close()
     
     # 폰트 지정
-    font = ImageFont.truetype("./NanumGothic.ttf",25)
+    font = ImageFont.truetype("./font/NanumGothic.ttf",25)
+    # 배경이 투명인 영역의 경계 상자값(왼쪽, 위쪽, 오른쪽, 아래쪽)
+    fontbox = font.getbbox(data)
+    fontbox_max = max(fontbox)
+    fontbox_width, fontbox_height = fontbox_max, fontbox_max
+    
+    # 이미지 사이즈 지정
+    text_width = fontbox_width * 2
+    text_height = fontbox_height * 2
+    
+    # 이미지 객체 생성
+    canvas = Image.new('RGB', (text_width, text_height), "white")
+    
+    # 캔버스에 글씨 입력하기
+    draw = ImageDraw.Draw(canvas)
+    draw.text((text_width/3, text_height/3), data, 'red', font)
+    
+    #png로 저장 및 출력해서 보기
+    canvas.save('rainbow.png', "PNG")
+    canvas.show()
     
     
 
